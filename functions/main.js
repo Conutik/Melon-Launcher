4070ca143850d0e6e59a0ccf24f66a174f73c8a8
Conutik <khawaj.zaid@gmail.com>
2021-10-11 20:58:56 +0300

    Update main.js

--- functions/main.js
+++ functions/main.js
@@ -9,12 +9,79 @@ function logSubmit () {
   const profiles = localStorage.getItem('profiles')
   const current = JSON.parse(localStorage.getItem('current'))
 
+  if(!current) {
+    localStorage.removeItem("current")
+    localStorage.removeItem("profiles")
+
+
+
+    ipc.send("open-login-menu")
+
+    window.close()
+    return;
+}
+
   let jre = localStorage.getItem('jrePath')
   let mcPath = localStorage.getItem('mcPath')
 
   if (jre) jre = process.env.JAVA_HOME
 
   if (!mcPath) mcPath = process.env.APPDATA + '/.minecraft'
+
+  if(current.mic) {
+    const { Client } = require('minecraft-launcher-core');
+        const launcher = new Client();
+
+        if(!current.Roles) current.Roles = []
+
+        let access = {
+            username: current.accessUser,
+            roles: current.accessRoles,
+            access_token: current.accessToken,
+            token_type: current.accessTokenType,
+            expires_in: current.accessExpiresIN
+        }
+
+        let acc = {
+            username: "Conutik",
+            uuid: "7b7ef169-ad97-4011-822f-ef64fc54f87f",
+            access_token: current.accessToken
+        }
+
+
+
+
+
+        let opts = {
+            clientPackage: null,
+            authorization: acc,
+            root: mcPath,
+            javaPath: jre,
+            version: {
+                number: "1.8.9",
+                type: "release"
+            },
+            memory: {
+                max: "4000M",
+                min: "4000M"
+            },
+            // window: {
+            // fullscreen: true
+            // width: window.screen.availWidth,
+            // height: window.screen.availHeight
+            // },
+            forge: "C:/Users/ziadk/Documents/coding/Melon Client/forge.jar"
+        }
+
+        launcher.launch(opts);
+
+        let i = 0
+
+        // launcher.on('debug', (e) => {});
+        launcher.on('data', (e) => {
+            document.getElementById('submit').innerHTML = "LAUNCHING";
+        })
+} else {
 
   auth.validate().then(x => {
     if (x === true) {
@@ -55,6 +122,7 @@ function logSubmit () {
     window.close()
   })
   // window.close()
+}
 }
 
 function appMinimize () {
